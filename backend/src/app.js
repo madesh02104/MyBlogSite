01f3a3f7656a7b923237b44028a61c2787f239d6
madesh02104 <madesh02104@gmail.com>
2025-04-28 21:59:16 +0530

    included admin route in user site

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,20 +6,16 @@ import commentRoutes from "./routes/commentRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 import { PrismaClient } from "@prisma/client";
 import dotenv from "dotenv";
-import configRouts from "./routes/configRoutes.js";
 
 dotenv.config();
 
 const app = express();
 
-console.log("DEBUG: FRONTEND_USER_URL:", process.env["FRONTEND_USER_URL"]);
-console.log("DEBUG: FRONTEND_ADMIN_URL:", process.env["FRONTEND_ADMIN_URL"]);
-
 const corsOptions = {
   origin: [process.env["FRONTEND_USER_URL"], process.env["FRONTEND_ADMIN_URL"]],
   credentials: true,
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Explicitly allow methods
-  allowedHeaders: "Content-Type,Authorization", // Explicitly allow requested headers
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  allowedHeaders: "Content-Type,Authorization",
 };
 
 // Middlewares
@@ -31,7 +27,7 @@ app.use(morgan("dev"));
 app.use("/api/posts", postRoutes);
 app.use("/api/comments", commentRoutes);
 app.use("/api/auth", authRoutes);
-app.use("/api/config", configRoutes);
+// Remove the config routes registration: app.use("/api/config", configRoutes);
 
 // 404 handler
 app.use((req, res) => {
