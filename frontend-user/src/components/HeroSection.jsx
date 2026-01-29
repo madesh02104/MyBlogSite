4f62ebde010cd31931b5da1d1cb499fad4d6958c
madesh02104 <madesh02104@gmail.com>
2026-01-29 15:52:26 +0530

    changed hero section

--- frontend-user/src/components/HeroSection.jsx
+++ frontend-user/src/components/HeroSection.jsx
@@ -2,25 +2,27 @@ function HeroSection() {
   return (
     <section className="bg-gray-800 py-16 text-white">
       <div className="container mx-auto px-8 md:px-48">
-        <h1 className="text-4xl md:text-5xl font-bold mb-6 text-violet-700 text-left">
+        <h1 className="text-5xl md:text-5xl font-bold mb-6 text-violet-700 text-left">
           👋 Hi there!
         </h1>
-        <p className="text-lg md:text-xl text-gray-300 mb-8 mx-auto text-justify">
+        <p className="text-lg md:text-3xl text-gray-300 mb-14 mx-auto text-justify">
           I've been in the tech space for little long enough to share something
           valuable to fellow tech enthusiasts.{" "}
-          <span className="text-red-700">Spoiler:</span> This blog will not
-          contain any{" "}
+          <span className="text-red-700">Spoiler:</span> This blog will not just
+          contain{" "}
           <span className="text-yellow-500">"How to create [this] app"</span> or{" "}
           <span className="text-yellow-500">"How i coded [that]" </span>
-          tutorials. I believe one can easily find those from official docs of
-          what you desire to learn. This blog will focus on my experiences in
-          development, strategic decisons, my methods of coding, architectures
-          etc.
+          tutorials. Beyond that i write about{" "}
+          <span className="text-green-500">
+            "How things work while we code stuff"
+          </span>
+          . These blogs will focus on my experiences in development, strategic
+          decisons, my methods of coding, architectures etc. Happy Reading : )
         </p>
         <div className="text-center">
           <a
             href="#blogs"
-            className="bg-violet-700 text-white py-3 px-6 rounded-lg hover:bg-violet-600 transition-all duration-200 transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-violet-500 focus:ring-offset-2 focus:ring-offset-gray-800"
+            className="bg-violet-700 text-xl text-white py-3 px-6 rounded-lg hover:bg-violet-600 transition-all duration-200 transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-violet-500 focus:ring-offset-2 focus:ring-offset-gray-800"
           >
             Explore Blogs
           </a>
