e6b2bd2e552b55a9cb9be0f88f94bdb779f31f96
madesh02104 <madesh02104@gmail.com>
2026-05-22 21:24:20 +0530

    fixed backend crash-looping

--- backend/src/app.js
+++ backend/src/app.js
@@ -63,7 +63,7 @@ app.use("/api/comments", commentRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/upload", uploadRoutes);
 
-app.get("/*", (req, res, next) => {
+app.use((req, res, next) => {
   if (req.path.startsWith("/api/")) {
     return next();
   }
